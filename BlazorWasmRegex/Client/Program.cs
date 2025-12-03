b45bebbe99d880cb60783ffce3de90c7ed63f39d
Michael Samorokov <michael.samorokov@gmail.com>
2025-12-02 18:25:38 -0700

    Fix root component selector and remove obsolete Cloudcrate script reference

--- BlazorWasmRegex/Client/Program.cs
+++ BlazorWasmRegex/Client/Program.cs
@@ -19,7 +19,7 @@ namespace BlazorWasmRegex.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<App>("app");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             ConfigureServices(builder.Services);
